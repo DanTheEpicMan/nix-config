bd9e47e7aff4cf42c0108f31e574c4f69515a346
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-05-24 18:40:38 -0400

    v9 - oops
    I forgot to enable vim in home manager so new version it is

--- home.nix
+++ home.nix
@@ -37,6 +37,10 @@
     vim
   ];
 
+   programs.vim = {
+    enable = true;
+   };
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
