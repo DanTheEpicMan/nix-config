639c91715bb971bc0f0277fdf0c4646bcbe221bf
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-05-24 09:51:20 -0400

    v5 - home manager
    added home manager and consequentially home.nix

--- flake.nix
+++ flake.nix
@@ -4,9 +4,18 @@
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     # Please replace my-nixos with your hostname
     nixosConfigurations.dan = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -14,6 +23,14 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
+
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.dan = import ./home.nix;
+        }
       ];
     };
   };
