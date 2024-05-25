6def178befcf6bfcde1533d147e1d4449fe2f6f0
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-05-25 12:50:38 -0400

    v9 - home manager as NixOS module
    installed home manager as a NixOS module, basically it is managed through configuration.nix but the code for it is housed in home.nix, home.nix is connected to the entrypoint (flake.nix) through configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,22 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.home-manager
     ];
+
+  #Ends up being the same as https://nix-community.github.io/home-manager/index.xhtml#sec-flakes-nixos-module
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      dan = import ./home.nix;
+    };
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
